5eb835ef0c5a4d341001c539ca83b9adb44cf6c5
ndoak <natalieannedoak@gmail.com>
2016-04-22 00:10:02 -0400

    Prevent user from entering empty post title or no post title

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ $scope.posts = [
 ];
 
 $scope.addPost = function(){
+  if(!$scope.title || $scope.title === '') { return; }
   $scope.posts.push({title: $scope.title, upvotes: 0});
   $scope.title = '';
 };
