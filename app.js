9bc8fb52db54dc9fd2630a5a671ecc8bc58838e9
ndoak <natalieannedoak@gmail.com>
2016-04-22 18:04:39 -0400

    Inject posts service into MainCtrl

--- app.js
+++ app.js
@@ -7,17 +7,19 @@ angular.module('newsApp', [])
 }])
 .controller('MainCtrl', [
   '$scope',
-function($scope){
+  'posts',
+
+function($scope,posts){
   $scope.test = "Hello World";
+  $scope.posts = posts.posts;
 
-
-$scope.posts = [
-  {title:'post 1', upvotes: 5},
-  {title:'post 2', upvotes: 2},
-  {title:'post 3', upvotes: 15},
-  {title:'post 4', upvotes: 9},
-  {title:'post 5', upvotes: 4}
-];
+// [
+//   {title:'post 1', upvotes: 5},
+//   {title:'post 2', upvotes: 2},
+//   {title:'post 3', upvotes: 15},
+//   {title:'post 4', upvotes: 9},
+//   {title:'post 5', upvotes: 4}
+// ];
 
 $scope.addPost = function(){
   if(!$scope.title || $scope.title === '') { return; }
